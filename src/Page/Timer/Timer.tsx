5200485992334073e38004d606f154c2aba8d244
ziqihuang <ziqihuang@6tiantian.com>
2020-06-11 13:49:26 +0800

    计时器就这样吧

--- src/Page/Timer/Timer.tsx
+++ src/Page/Timer/Timer.tsx
@@ -1,49 +1,46 @@
 import * as React from 'react';
-import {formatDuration} from "../../Utils/TimeUtils";
-import "./Timer.scss"
+import { formatDuration } from '../../Utils/TimeUtils';
+import './Timer.scss';
 
 interface TimerState {
-    timeStamp: number
+  timeStamp: number;
 }
 
 export default class Timer extends React.Component<any, TimerState> {
-    private static startTime: number = 1591852623000;
-    private tn: any;
+  private static startTime: number = 1591852623000;
+  private tn: any;
 
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            timeStamp: 0,
-        }
-    }
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      timeStamp: 0
+    };
+  }
 
-    componentDidMount(): void {
-        this.setState({
-            timeStamp: Date.now() - Timer.startTime
-        });
+  componentDidMount(): void {
+    this.setState({
+      timeStamp: Date.now() - Timer.startTime
+    });
 
-        this.tn = setInterval(() => {
-            this.setState({
-                timeStamp: this.state.timeStamp + 1000
-            })
-        }, 1000)
-    }
+    this.tn = setInterval(() => {
+      this.setState({
+        timeStamp: this.state.timeStamp + 1000
+      });
+    }, 1000);
+  }
 
-    componentWillUnmount(): void {
-        clearInterval(this.tn);
-    }
+  componentWillUnmount(): void {
+    clearInterval(this.tn);
+  }
 
-    render(): React.ReactNode {
-        return <div className={"root"}>
-            <div className={"timeContainer"}>
-               <div className={"text"}>
-                   {formatDuration(this.state.timeStamp,"ddd")}
-               </div>
-                <div className={"text"}>
-                    {formatDuration(this.state.timeStamp,"hh:mm:ss")}
-                </div>
-            </div>
+  render(): React.ReactNode {
+    return (
+      <div className={'root'}>
+        <div className={'timeContainer'}>
+          <div className={'text'}>{formatDuration(this.state.timeStamp, 'ddd')}</div>
+          <div className={'text'}>{formatDuration(this.state.timeStamp, 'hh:mm:ss')}</div>
         </div>
-    }
-
+      </div>
+    );
+  }
 }
--- src/TimeUpdate/TimeUpdate.jsx
+++ src/TimeUpdate/TimeUpdate.jsx
@@ -1,44 +1,42 @@
 import React from 'react';
-import {formatDuration} from "../Utils/TimeUtils";
-import './TimeUpdate.css'
+import { formatDuration } from '../Utils/TimeUtils';
+import './TimeUpdate.css';
 
-const styles={
-    text: {
-        color: "#ecb1c9",
-        fontSize:"10vw",
-        fontFamily:"XiangSu",
-    }
+const styles = {
+  text: {
+    color: '#ecb1c9',
+    fontSize: '10vw',
+    fontFamily: 'XiangSu'
+  }
 };
 
-
 class TimeUpdate extends React.Component {
-    interval;
-    constructor(props) {
-        super(props);
-        const nowTime = new Date().getTime();
-        this.state={
-            remindTime: props.finalTime - nowTime
-        }
+  interval;
+  constructor(props) {
+    super(props);
+    const nowTime = new Date().getTime();
+    this.state = {
+      remindTime: props.finalTime - nowTime
+    };
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        remindTime: this.state.remindTime - 90
+      });
+    }, 90);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
-    }
-    componentDidMount() {
-        this.interval = setInterval(()=>{
-            this.setState({
-                remindTime : this.state.remindTime - 90
-            })
-        },90)
-    }
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-
-    render() {
-        const str=formatDuration(this.state.remindTime,"h:mm:ss.S");
-        return <div className={"TimeUpdate"}>
-            <div style={styles.text}>
-                { str }
-            </div>
-        </div>
-    }
+  render() {
+    const str = formatDuration(this.state.remindTime, 'h:mm:ss.S');
+    return (
+      <div className={'TimeUpdate'}>
+        <div style={styles.text}>{str}</div>
+      </div>
+    );
+  }
 }
 export default TimeUpdate;
