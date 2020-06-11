a8efac6696293b8aba8a489d32c84bfdc84fdc9c
ziqihuang <ziqihuang@6tiantian.com>
2020-06-11 13:49:02 +0800

    计时器就这样吧

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Sakura from "./Page/Sakura/Sakura";
 import ChildrenDay from "./Page/ChildrenDay/ChildrenDay";
+import Timer from "./Page/Timer/Timer";
 
 function App() {
 
   return (
     <div className="App">
       {/*<Sakura/>*/}
-        <ChildrenDay/>
+      {/*  <ChildrenDay/>*/}
+      <Timer/>
     </div>
   );
 }
---
+++ src/Page/Timer/Timer.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import {formatDuration} from "../../Utils/TimeUtils";
+import "./Timer.scss"
+
+interface TimerState {
+    timeStamp: number
+}
+
+export default class Timer extends React.Component<any, TimerState> {
+    private static startTime: number = 1591852623000;
+    private tn: any;
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            timeStamp: 0,
+        }
+    }
+
+    componentDidMount(): void {
+        this.setState({
+            timeStamp: Date.now() - Timer.startTime
+        });
+
+        this.tn = setInterval(() => {
+            this.setState({
+                timeStamp: this.state.timeStamp + 1000
+            })
+        }, 1000)
+    }
+
+    componentWillUnmount(): void {
+        clearInterval(this.tn);
+    }
+
+    render(): React.ReactNode {
+        return <div className={"root"}>
+            <div className={"timeContainer"}>
+               <div className={"text"}>
+                   {formatDuration(this.state.timeStamp,"ddd")}
+               </div>
+                <div className={"text"}>
+                    {formatDuration(this.state.timeStamp,"hh:mm:ss")}
+                </div>
+            </div>
+        </div>
+    }
+
+}
